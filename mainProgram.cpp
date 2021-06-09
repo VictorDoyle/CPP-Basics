4698ac9513e52237a790243873bc5870367696c8
victordoyle <victor-doyle@outlook.com>
2021-06-09 19:23:26 -0400

    setting up narrative for locked door game

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
 int main(){
+    /* std = namespace;   :: is the scope operator ; c out is the output stream to terminal */
+    std::cout << "You are approaching a dark corridor with a door at its end.";
+    std::cout << std::endl;
+    std::cout << "The door is locked by means of padlock with a secret combination.";
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct combination to pass";
+    std::cout << std::endl;
     return 0;
 }
